791f4fc32b73ec290455578b50a31661578e218c
xanndevs <canberkcakir04@gmail.com>
2024-05-16 20:16:13 +0300

    SQL connection test.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from flask import render_template
 from app import app
 
+from app.models import User
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', content="AZAAAAAAAAAAAAAA")
+
+
+
+@app.route("/aglaindex")
+def aindex():
+    users = User.query.all()
+    user = users[0]
+    return str(user.id) + user.username + user.email
