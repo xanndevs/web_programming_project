d73e0c54f28f0e133d3ea276ed5f17be54f02494
xanndevs <canberkcakir04@gmail.com>
2024-05-25 18:32:07 +0300

    deployment test 2

--- config.py
+++ config.py
@@ -8,3 +8,15 @@ class Config(object):
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = basedir+"/uploads"
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
