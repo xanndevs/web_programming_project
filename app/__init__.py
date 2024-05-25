86875401fd5b8a4176f22ccd573cfb849fb0df7a
xanndevs <canberkcakir04@gmail.com>
2024-05-25 23:00:38 +0300

    File size limit adjusted.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100MB in bytes
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
